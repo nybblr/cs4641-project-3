84d8230568403696f02eb1849eee1c1eeba94cc8
Jonathan Martin <me@nybblr.com>
2013-03-30 16:57:42 -0400

    Pulling out some classes for easier use. Fixed classpath for my machine. Added reduce worker just to run filtering.

--- src/ProjectRunner.java
+++ src/ProjectRunner.java
@@ -1,10 +1,10 @@
-/*import java.io.IOException;
-import java.util.ArrayList;*/
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-/*import dist.AbstractConditionalDistribution;
+import dist.AbstractConditionalDistribution;
 
 import func.FunctionApproximater;
 import func.KMeansClusterer;
@@ -17,11 +17,11 @@ import shared.filt.LabelSplitFilter;
 import shared.filt.PrincipalComponentAnalysis;
 import shared.filt.RandomizedProjectionFilter;
 import shared.filt.ReversibleFilter;
-import shared.reader.ArffDataSetReader;*/
+import shared.reader.ArffDataSetReader;
 import shared.DataSet;
 import shared.filt.LabelSelectFilter;
 import shared.reader.ArffDataSetReader;
-//import shared.reader.CSVDataSetReader;
+import shared.reader.CSVDataSetReader;
 import shared.reader.CSVDataSetReader;
 
 public class ProjectRunner {
@@ -92,68 +92,4 @@ public class ProjectRunner {
 		
 	}
 	
-	/**
-	 * Not in use any more, but kept around for the filtering code as a reference
-	 * @author chronon
-	 *
-	 */
-	/*private static class DataSetWorker {
-		// static variables
-		private static final String reducedDir = "data/reduced/";
-		private static final String clustReducedDir = "data/creduced/";
-		private static final String[] reduced = {"_pca.csv", "_ica.csv", "_insig.csv", "_rp.csv"};
-		private static final String[] clustered = {"_kmeans", "_emax"};
-		
-		// the array of DataSets corresponding to the mountain of nnets we need to train
-		private DataSet clean;
-
-		private String setName;
-		private final int toKeep = 5;
-		
-		
-		public DataSetWorker(String setName) {
-			this.setName = setName;
-		}
-		
-		public void reduce() {
-	    	// Add all the filters we need
-			ArrayList<Tuple<ReversibleFilter,String>> filters = new ArrayList<Tuple<ReversibleFilter,String>>();
-	        filters.add(new Tuple<ReversibleFilter, String>(new PrincipalComponentAnalysis(clean), "_pca.csv"));
-	        filters.add(new Tuple<ReversibleFilter, String>(new IndependentComponentAnalysis(clean), "_ica.csv"));
-	        filters.add(new Tuple<ReversibleFilter, String>(new RandomizedProjectionFilter(toKeep, clean.get(0).size()), "_rp.csv"));
-	        filters.add(new Tuple<ReversibleFilter, String>(new InsignificantComponentAnalysis(clean), "_insig.csv"));
-			for (Tuple<ReversibleFilter, String> tup : filters) {
-				ReversibleFilter filter = tup.fst();
-				String ext = tup.snd();
-				
-				filter.filter(clean);
-				DataSetWriter wr = new DataSetWriter(clean, reducedDir+setName+ext);
-				try {
-					wr.write();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				filter.reverse(clean);
-			}
-		}
-
-		
-	}
-	
-	public static class Tuple<X, Y> { 
-		  private final X fst; 
-		  private final Y snd; 
-		  public Tuple(X x, Y y) { 
-		    this.fst = x; 
-		    this.snd = y; 
-		  } 
-		  
-		  public X fst() {
-			  return this.fst;
-		  }
-		  
-		  public Y snd() {
-			  return this.snd;
-		  }
-		} */
 }
